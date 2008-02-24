9437521bebf611c8dac6947857d7af3db2e80184
sotn3m <sotn3m@fd091c19-b246-0410-b153-9379a4bf11a3>
2008-02-24 21:38:38 +0000

    Initial creature parameters import.

--- Pinklife/src/data/CreatureBean.java
+++ Pinklife/src/data/CreatureBean.java
@@ -10,5 +10,59 @@ package data;
  * @author sotn3m <sotn3m at gmail dot com>
  */
 public class CreatureBean {
+    private String sName;    
+    private int iHappiness;
+    private int iFoodLevel;
+    private int iWaterLevel;
+    private int iDirtyLevel;
+    private int iPlayLevel;
+
+    public String getName() {
+        return sName;
+    }
+
+    public void setName(String name) {
+        this.sName = name;
+    }
+
+    public int getIHappiness() {
+        return iHappiness;
+    }
+
+    public void setIHappiness(int iHappiness) {
+        this.iHappiness = iHappiness;
+    }
+
+    public int getIFoodLevel() {
+        return iFoodLevel;
+    }
+
+    public void setIFoodLevel(int iFoodLevel) {
+        this.iFoodLevel = iFoodLevel;
+    }
+
+    public int getIWaterLevel() {
+        return iWaterLevel;
+    }
+
+    public void setIWaterLevel(int iWaterLevel) {
+        this.iWaterLevel = iWaterLevel;
+    }
+
+    public int getIDirtyLevel() {
+        return iDirtyLevel;
+    }
+
+    public void setIDirtyLevel(int iDirtyLevel) {
+        this.iDirtyLevel = iDirtyLevel;
+    }
+
+    public int getIPlayLevel() {
+        return iPlayLevel;
+    }
+
+    public void setIPlayLevel(int iPlayLevel) {
+        this.iPlayLevel = iPlayLevel;
+    }
     
 }
